a9a178761e68f9d0d5db357f48fccc659d5dee31
matcha-bunny <shcrtcxke@gmail.com>
2025-02-12 13:35:14 -0600

    Update 2 config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://open.spotify.com/track/2eAvDnpXP5W0cVtiI0PUxV?si=42d0a2e3764b4eda", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dnaqxlevu/video/upload/v1739388817/pjfik6ujcykmms8kqki1.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
