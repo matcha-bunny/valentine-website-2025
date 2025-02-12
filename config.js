78a120ddbac461d3d9aa351eeb402200cbbf759f
matcha-bunny <shcrtcxke@gmail.com>
2025-02-12 14:03:38 -0600

    Update 7 config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ const CONFIG = {
     celebration: {
         title: "YAY am the luckiest in the world! 🎉💝💖🖤💓",
         message: "come get your gift;> hug and a big kiss!",
-        emojis: "🎁🎸🤗💴💋🍣🖤🥤🩷🐮💝🍟🩷🍫🖤"  // These will bounce around
+        emojis: "🎁🎸🖤💴💋🍣🖤🥤🩷🐮💝🍟🩷🍫🤗"  // These will bounce around
     },
 
     // Color scheme for the website
@@ -67,8 +67,8 @@ const CONFIG = {
     // Animation settings
     // Adjust these if you want faster/slower animations
     animations: {
-        floatDuration: "15s",           // How long it takes hearts to float up (10-20s recommended)
-        floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
+        floatDuration: "10s",           // How long it takes hearts to float up (10-20s recommended)
+        floatDistance: "45px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
         heartExplosionSize: 2.0         // Size of heart explosion effect (1.2-2.0 recommended)
     },
